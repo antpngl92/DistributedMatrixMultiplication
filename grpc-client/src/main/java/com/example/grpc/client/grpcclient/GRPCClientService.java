8c92b020374575fa7a3075ce4a5fb510e42364da
jdoyle314 <35959450+jdoyle314@users.noreply.github.com>
2021-03-24 10:24:03 +0000

    Add files via upload

---
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -0,0 +1,25 @@
+package com.example.grpc.client.grpcclient;
+
+import com.example.grpc.server.grpcserver.PingRequest;
+import com.example.grpc.server.grpcserver.PongResponse;
+import com.example.grpc.server.grpcserver.PingPongServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+@Service
+public class GRPCClientService {
+    public String ping() {
+        	ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+                .usePlaintext()
+                .build();        
+		PingPongServiceGrpc.PingPongServiceBlockingStub stub
+                = PingPongServiceGrpc.newBlockingStub(channel);        
+		PongResponse helloResponse = stub.ping(PingRequest.newBuilder()
+                .setPing("")
+                .build());        
+		channel.shutdown();        
+		return helloResponse.getPong();
+    }
+}
